f238545da28568f4e8175fab7a35ce4dae5020db
Mohsin Malik <mohsinmalik324@gmail.com>
2016-11-28 19:14:58 -0500

    Start menu for asmdb

--- computer_science_ii/asmdb/src/org/mohsinmalik324/asmdb/ASMDB.java
+++ computer_science_ii/asmdb/src/org/mohsinmalik324/asmdb/ASMDB.java
@@ -1,5 +1,7 @@
 package org.mohsinmalik324.asmdb;
 
+import java.util.Scanner;
+
 /**
  * The main driver class.
  * 
@@ -15,12 +17,55 @@ package org.mohsinmalik324.asmdb;
  */
 public class ASMDB {
 	
+	private static Scanner scanner = null;
+	
 	/**
 	 * Main method.
 	 * @param args Arguments.
 	 */
 	public static void main(String[] args) {
+		scanner = new Scanner(System.in);
 		
+		
+		
+		scanner.close();
+	}
+	
+	/**
+	 * Prints the main menu options.
+	 */
+	public static void printMainMenu() {
+		println("Main Menu:");
+		println("\tI: Import Movie <Title>");
+		println("\tD: Delete Movie <Title>");
+		println("\tS: Sort Movies");
+		println("\tA: Sort Actors");
+		// TODO
+	}
+	
+	/**
+	 * Gets user input.
+	 * @param prompt The prompt displayed to the user.
+	 * @return The user input.
+	 */
+	public static String getInput(String prompt) {
+		return scanner.nextLine();
+	}
+	
+	/**
+	 * Prints a message to stdout with a new line charactor.
+	 * @param message The message to print.
+	 */
+	public static void println(String message) {
+		System.out.println(message);
+	}
+	
+	/**
+	 * Prints a message to stdout.
+	 * @param message The message to print.
+	 */
+	public static void print(String message) {
+		System.out.print(message);
 	}
 	
 }
