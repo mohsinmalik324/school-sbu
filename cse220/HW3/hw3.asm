542ead67ee957f8106c62a58e8e1f3429228dba5
Mohsin Malik <mohsinmalik324@gmail.com>
2017-11-10 11:45:36 -0500

    Finish HW3 for CSE220

--- cse220/HW3/hw3.asm
+++ cse220/HW3/hw3.asm
@@ -1,9 +1,9 @@
-
 ##############################################################
 # Homework #3
 # name: Mohsin Malik
 # sbuid: 100880864
 ##############################################################
+
 .text
 
 # a0 - address of the string
@@ -432,7 +432,14 @@ printDatagram_done:
 # a3 - size of each packet in the array
 extractUnorderedData:
 	# save any necessary registers onto the stack
-	addi $sp $sp -8
+	addi $sp $sp -36
+	sw $s7 32($sp)
+	sw $s6 28($sp)
+	sw $s5 24($sp)
+	sw $s4 20($sp)
+	sw $s3 16($sp)
+	sw $s2 12($sp)
+	sw $s1 8($sp)
 	sw $s0 4($sp)
 	sw $ra 0($sp)
 
@@ -443,6 +450,12 @@ extractUnorderedData:
 	
 extractUnorderedData_1:
 	move $s0 $a0 # s0 = a0
+	jal verifyIPv4Checksum
+	beqz $v0 extractUnorderedData_1_cg # v0 == 0
+	j extractUnorderedData_1_err
+
+extractUnorderedData_1_cg: # cg = checksum good
+	move $a0 $s0 # a0 = s0
 	jal getFlags
 	li $t0 2 # t0 = 2
 	beq $v0 $t0 extractUnorderedData_1_nf # v0 == 2
@@ -450,25 +463,147 @@ extractUnorderedData_1:
 	j extractUnorderedData_err
 	
 extractUnorderedData_1_flag0:
+	move $a0 $s0 # a0 = s0
 	jal getFragmentOffset
-	beqz $v0 extractUnorderedData_1_nf
+	beqz $v0 extractUnorderedData_1_nf # v0 == 0
 	j extractUnorderedData_err
 	
 extractUnorderedData_1_nf: # nf = not fragmented
-	jal verifyIPv4Checksum
-	beqz $v0 extractUnorderedData_1_cg
-	li $v0 -1 # v0 = -1
-	li $v1 0 # v1 = 0
-	j extractUnorderedData_exit
+	move $a0 $s0 # a0 = s0
+	move $s1 $a1 # s1 = a1
+	move $a1 $a3 # a1 = a3
+	jal checkTotalLength
+	beqz $v0 extractUnorderedData_1_tlgtpes # v0 == 0
+	j extractUnorderedData_1_err
 	
-extractUnorderedData_1_cg: # cg = checksum good
+extractUnorderedData_1_tlgtpes: # tlgtpes = total length greater than packet entry size
+	# s2 - payload size
+	# s3 - header length in bytes
+	# s4 - starting address of byte array
+	move $a0 $s0 # a0 = s0
+	jal calcPayloadSize
+	move $s2 $v0 # s2 = v0
+	move $a0 $s0 # a0 = s0
+	jal getHeaderLength
+	move $s3 $v0 # s3 = v0
+	li $t0 4 # t0 = 4
+	mul $s3 $s3 $t0 # s3 = s3 * 4
 	
-
+	move $s4 $a2 # s4 = a2
+	move $s5 $a3 # s5 = a3
+	
+	move $a0 $s0 # a0 = s0
+	add $a0 $a0 $s3 # a0 = a0 + s3
+	move $a1 $a2 # a1 = a2
+	li $a2 0 # a2 = 0
+	move $a3 $s2 # a3 = s2
+	jal storeMessageIntoArray
+	
+	move $v1 $v0 # v1 = v0
+	li $v0 0 # v0 = 0
+	
 	j extractUnorderedData_exit
 	
 extractUnorderedData_n:
+	# s0 - counter
+	# s1 - packet array
+	# s2 - n
+	# s3 - byte array
+	# s4 - size of each packet element in array
+	# s5 - start packet counter
+	# s6 - end packet counter
+	# s7 - highest stored index
 	
-
+	move $s1 $a0 # s1 = a0
+	move $s2 $a1 # s2 = a1
+	move $s3 $a2 # s3 = a2
+	move $s4 $a3 # s4 = a3
+	li $s0 0 # s0 = 0
+	li $s5 0 # s5 = 0
+	li $s6 0 # s6 = 0
+	li $s7 -1 # s7 = -1
+	
+extractUnorderedData_n_loop:
+	bge $s0 $s2 extractUnorderedData_n_done # s0 >= $s2
+	
+	move $a0 $s1 # a0 = s1
+	jal verifyIPv4Checksum
+	bnez $v0 extractUnorderedData_n_err # v0 != 0
+	move $a0 $s1 # a0 = s1
+	move $a1 $s4 # a1 = s4
+	jal checkTotalLength
+	bnez $v0 extractUnorderedData_n_err # v0 != 0
+	move $a0 $s1 # a0 = s1
+	jal getPacketType
+	li $t0 -1 # t0 = -1
+	beq $v0 $t0 extractUnorderedData_err # v0 == -1
+	beqz $v0 extractUnorderedData_n_loop_start # v0 == 0
+	li $t0 2 # t0 = 2
+	beq $v0 $t0 extractUnorderedData_n_loop_end # v0 == 2
+	j extractUnorderedData_n_loop_after
+extractUnorderedData_n_loop_start:
+	addi $s5 $s5 1 # s5 = s5 + 1
+	j extractUnorderedData_n_loop_after
+extractUnorderedData_n_loop_end:
+	addi $s6 $s6 1 # s6 = s6 + 1
+extractUnorderedData_n_loop_after:
+	move $a0 $s1 # a0 = s1
+	jal getWhereToStore
+	li $t0 -1 # t0 = -1
+	beq $v0 $t0 extractUnorderedData_err # v0 == -1
+	
+	# save index to store on stack (ran out of s registers)
+	addi $sp $sp -4
+	sw $v0 0($sp)
+	
+	move $a0 $s1 # a0 = s1
+	jal getHeaderLength
+	li $t0 4 # t0 = 4
+	mul $v0 $v0 $t0 # v0 = v0 * 4 - get the header length in bytes
+	
+	# save the header length on the stack (ran out of s registers)
+	addi $sp $sp -4
+	sw $v0 0($sp)
+	
+	move $a0 $s1 # a0 = s1
+	jal calcPayloadSize
+	
+	# get saved variables from stack
+	lw $t0 0($sp) # header length in bytes
+	lw $t1 4($sp) # index
+	addi $sp $sp 8
+	
+	add $a0 $s1 $t0 # a0 = s1 + t0 - get starting address of the payload
+	move $a1 $s3 # a1 = s3
+	move $a2 $t1 # a2 = t1
+	move $a3 $v0 # a3 = v0
+	jal storeMessageIntoArray
+	ble $v0 $s7 extractUnorderedData_n_skip # v0 <= s7
+	move $s7 $v0
+	
+extractUnorderedData_n_skip:
+	
+	add $s1 $s1 $s4 # s1 = s1 + s4
+	addi $s0 $s0 1 # s0 = s0 + 1
+	
+	j extractUnorderedData_n_loop
+	
+extractUnorderedData_n_err:
+	li $v0 -1 # v0 = -1
+	move $v1 $s0 # v1 = s0
+	j extractUnorderedData_exit
+	
+extractUnorderedData_n_done:
+	li $t0 1 # t0 = 1
+	bne $s5 $t0 extractUnorderedData_err # s5 != 1
+	bne $s6 $t0 extractUnorderedData_err # s6 != 1
+	li $v0 0 # v0 = 0
+	move $v1 $s7 # v1 = s7
+	j extractUnorderedData_exit
+	
+extractUnorderedData_1_err:
+	li $v0 -1 # v0 = -1
+	li $v1 0 # v1 = 0
 	j extractUnorderedData_exit
 	
 extractUnorderedData_err:
@@ -480,9 +615,87 @@ extractUnorderedData_exit:
 	# restore registers
 	lw $ra 0($sp)
 	lw $s0 4($sp)
-	addi $sp $sp 8
+	lw $s1 8($sp)
+	lw $s2 12($sp)
+	lw $s3 16($sp)
+	lw $s4 20($sp)
+	lw $s5 24($sp)
+	lw $s6 28($sp)
+	lw $s7 32($sp)
+	addi $sp $sp 36
 
 	jr $ra
+	
+# a0 - ipv4 packet array
+# a1 - number of packets in packet array
+# a2 - byte array
+# a3 - string array
+# a4(on stack) - packetentrysize
+printUnorderedDatagram:
+	lw $s4 0($sp) # load fifth argument from stack
+
+	addi $sp $sp -24
+	sw $ra 20($sp)
+	sw $s4 16($sp)
+	sw $s3 12($sp)
+	sw $s2 8($sp)
+	sw $s1 4($sp)
+	sw $s0 0($sp)
+	
+	# s0 - ipv4 packet array
+	# s1 - number of packets in packet array
+	# s2 - byte array
+	# s3 - string array
+	# s4 - packetentrysize
+	
+	move $s0 $a0 # s0 = a0
+	move $s1 $a1 # s1 = a1
+	move $s2 $a2 # s2 = a2
+	move $s3 $a3 # s3 = a3
+	
+	move $a0 $s0 # a0 = s0
+	move $a1 $s1 # a1 = s1
+	move $a2 $s2 # a2 = s2
+	move $a3 $s4 # a3 = s4
+	
+	jal extractUnorderedData
+	beq $v0 -1 printUnorderedDatagram_err # v0 == -1
+	
+	move $a0 $s2 # a0 = s2
+	move $a1 $v1 # a1 = v1
+	move $a2 $s3 # a2 = a3
+	
+	jal processDatagram
+	beq $v0 -1 printUnorderedDatagram_err # v0 == -1
+	
+	move $a0 $s3 # a0 = s3
+	li $a1 0 # a0 = 0
+	move $a2 $v0 # a2 = v0
+	addi $a2 $a2 -1 # a2 = a2 - 1
+	move $a3 $v0 # a3 = v0
+	
+	jal printStringArray
+	beq $v0 -1 printUnorderedDatagram_err # v0 == -1
+	
+	li $v0 0 # v0 = 0
+	
+	j printUnorderedDatagram_done
+	
+printUnorderedDatagram_err:
+	li $v0 -1
+	j printUnorderedDatagram_done
+	
+printUnorderedDatagram_done:
+	lw $s0 0($sp)
+	lw $s1 4($sp)
+	lw $s2 8($sp)
+	lw $s3 12($sp)
+	lw $s4 16($sp)
+	lw $ra 20($sp)
+	addi $sp $sp 24
+
+	jr $ra
+	
 	
 # a0 - address of the packet
 getFlags:
@@ -512,21 +725,273 @@ getHeaderLength:
 	
 # a0 - the packet
 calcPayloadSize:
-	addi $sp $sp -4
+	addi $sp $sp -12
+	sw $s1 8($sp)
+	sw $s0 4($sp)
 	sw $ra 0($sp)
+	
+	move $s0 $a0 # s0 = a0
 	
 	jal getHeaderLength
 	li $t0 4 # t0 = 4
-	mul $t1 $v0 $t0 # t1 = v0 * 4
+	mul $s1 $v0 $t0 # s1 = v0 * 4
 	
+	move $a0 $s0
 	jal getTotalLength
-	sub $v0 $v0 $t1 # v0 = v0 - t1
+	sub $v0 $v0 $s1 # v0 = v0 - s1
 	
 	lw $ra 0($sp)
-	addi $sp $sp 4
+	lw $s0 4($sp)
+	lw $s1 8($sp)
+	addi $sp $sp 12
 	
 	jr $ra
 	
+# a0 - packet address
+# a1 - packetentrysize
+checkTotalLength:
+	lhu $t0 0($a0) # a0 -> t0
+	bgt $t0 $a1 checkTotalLength_gt # t0 > a1
+	li $v0 0 # v0 = 0
+	jr $ra
+	
+checkTotalLength_gt:
+	li $v0 1 # v0 = 1
+	jr $ra
+	
+# a0 - starting address of message
+# a1 - byte array
+# a2 - index to start storing
+# a3 - message size
+storeMessageIntoArray:
+	add $a1 $a1 $a2 # a1 = a1 + a2 - move the pointer to where we want to start storing data
+	li $t1 0
+	
+storeMessageIntoArray_loop:
+	bge $t1 $a3 storeMessageIntoArray_exit # t1 >= a3
+	lbu $t0 0($a0) # a0 -> t0
+	sb $t0 0($a1) # t0 -> a1
+	addi $a0 $a0 1 # a0 = a0 + 1
+	addi $a1 $a1 1 # a1 = a1 + 1
+	addi $t1 $t1 1 # t1 = t1 + 1
+	j storeMessageIntoArray_loop
+	
+storeMessageIntoArray_exit:
+	move $v0 $t1 # v0 = t1
+	add $v0 $v0 $a2 # v0 = v0 + a2
+	jr $ra
+	
+# a0 - packet
+getPacketType:
+	addi $sp $sp -16
+	sw $s2 12($sp)
+	sw $s1 8($sp)
+	sw $s0 4($sp)
+	sw $ra 0($sp)
+	
+	# s0 - packet
+	# s1 - flags
+	# s2 - fragment offset
+	
+	move $s0 $a0 # s0 = a0
+	jal getFlags
+	move $s1 $v0 # s1 = v0
+	move $a0 $s0 # a0 = s0
+	jal getFragmentOffset
+	move $s2 $v0 # s2 = v0
+	
+	li $t0 4 # t0 = 4
+	beq $s1 $t0 getPacketType_flag4 # s1 == 4
+	beqz $s1 getPacketType_flag0 # s1 == 0
+	
+	j getPacketType_err
+	
+getPacketType_flag0:
+	bnez $s2 getPacketType_end # s2 != 0
+	j getPacketType_err
+	
+getPacketType_flag4:
+	beqz $s2 getPacketType_start # s2 == 0
+	j getPacketType_middle
+	
+getPacketType_start:
+	li $v0 0 # v0 = 0
+	j getPacketType_exit
+
+getPacketType_middle:
+	li $v0 1 # v0 = 1
+	j getPacketType_exit
+
+getPacketType_end:
+	li $v0 2 # v0 = 2
+	j getPacketType_exit
+	
+getPacketType_err:
+	li $v0 -1 # v0 = -1
+	j getPacketType_exit
+	
+getPacketType_exit:
+	lw $ra 0($sp)
+	lw $s0 4($sp)
+	lw $s1 8($sp)
+	lw $s2 12($sp)
+	addi $sp $sp 16
+	
+	jr $ra
+	
+# a0 - packet
+getWhereToStore:
+	addi $sp $sp -16
+	sw $s2 12($sp)
+	sw $s1 8($sp)
+	sw $s0 4($sp)
+	sw $ra 0($sp)
+	
+	# s0 - packet
+	# s1 - packet type
+	# s2 - fragment offset
+	
+	move $s0 $a0 # s0 = a0
+	jal getPacketType
+	li $t0 -1 # t0 = -1
+	beq $v0 $t0 getWhereToStore_exit # v0 == -1
+	move $s1 $v0 # s1 = v0
+	move $a0 $s0 # a0 = s0
+	jal getFragmentOffset
+	move $s2 $v0 # s2 = v0
+	
+	beqz $s1 getWhereToStore_start # s1 == 0
+	j getWhereToStore_other
+	
+getWhereToStore_start:
+	li $v0 0 # v0 = 0
+	j getWhereToStore_exit
+	
+getWhereToStore_other:
+	move $v0 $s2 # v0 = s2
+	j getWhereToStore_exit
+
+getWhereToStore_exit:
+	lw $ra 0($sp)
+	lw $s0 4($sp)
+	lw $s1 8($sp)
+	lw $s2 12($sp)
+	addi $sp $sp 16
+	jr $ra
+	
+# a0 - str1
+# a1 - str2
+# a2 - length of str1 (m)
+# a3 - length of str2 (n)
+editDistance:
+	addi $sp $sp -32
+	sw $s6 28($sp)
+	sw $s5 24($sp)
+	sw $s4 20($sp)
+	sw $s3 16($sp)
+	sw $s2 12($sp)
+	sw $s1 8($sp)
+	sw $s0 4($sp)
+	sw $ra 0($sp)
+	
+	# s0 - str1
+	# s1 - str2
+	# s2 - m (str1 len)
+	# s3 - n (str2 len)
+	move $s0 $a0 # s0 = a0
+	move $s1 $a1 # s1 = a1
+	move $s2 $a2 # s2 = a2
+	move $s3 $a3 # s3 = a3
+	
+	bltz $s2 editDistance_ret_neg1 # s2 < 0
+	bltz $s3 editDistance_ret_neg1 # s3 < 0
+	beqz $s2 editDistance_ret_n # s2 == 0
+	beqz $s3 editDistance_ret_m # s3 == 0
+	
+	addi $t0 $s2 -1 # t0 = s2 - 1
+	addi $t1 $s3 -1 # t1 = s3 - 1
+	add $t2 $s0 $t0 # t2 = s0 + t0
+	add $t3 $s1 $t1 # t3 = s1 + t1
+	
+	lb $t4 0($t2) # t2 -> t4
+	lb $t5 0($t3) # t3 -> t5
+	
+	beq $t4 $t5 editDistance_il
+	
+	# s4 - insert
+	# s5 - remove
+	# s6 - replace
+	move $a0 $s0 # a0 = s0
+	move $a1 $s1 # a1 = s1
+	move $a2 $s2 # a2 = s2
+	addi $a3 $s3 -1 # a3 = s3 - 1
+	jal editDistance
+	move $s4 $v0 # s4 = v0
+	
+	move $a0 $s0 # a0 = s0
+	move $a1 $s1 # a1 = s1
+	addi $a2 $s2 -1 # a2 = s2 - 1
+	move $a3 $s3 # a3 = s3
+	jal editDistance
+	move $s5 $v0 # s5 = v0
+	
+	move $a0 $s0 # a0 = s0
+	move $a1 $s1 # a1 = s1
+	addi $a2 $s2 -1 # a2 = s2 - 1
+	addi $a3 $s3 -1 # a3 = s3 - 1
+	jal editDistance
+	move $s6 $v0 # s6 = v0
+	
+	move $a0 $s4 # a0 = s4
+	move $a1 $s5 # a1 = s5
+	jal getMin
+	move $a0 $v0 # a0 = v0
+	move $a1 $s6 # a1 = s6
+	jal getMin
+	addi $v0 $v0 1 # v0 = v0 + 1
+	j editDistance_exit
+	
+editDistance_il: # il = ignore last
+	move $a2 $t0 # a2 = t0
+	move $a3 $t1 # a3 = t1
+	jal editDistance
+	j editDistance_exit
+	
+editDistance_ret_neg1:
+	li $v0 -1 # v0 = -1
+	j editDistance_exit
+	
+editDistance_ret_m:
+	move $v0 $s2 # v0 = s2
+	j editDistance_exit
+	
+editDistance_ret_n:
+	move $v0 $s3 # v0 = s3
+	j editDistance_exit
+	
+editDistance_exit:
+	lw $ra 0($sp)
+	lw $s0 4($sp)
+	lw $s1 8($sp)
+	lw $s2 12($sp)
+	lw $s3 16($sp)
+	lw $s4 20($sp)
+	lw $s5 24($sp)
+	lw $s6 28($sp)
+	addi $sp $sp 32
+	
+	jr $ra
+	
+# a0 - fnum
+# a1 - snum
+getMin:
+	blt $a0 $a1 getMin_ret_a0 # a0 < a1
+	move $v0 $a1 # v0 = a1
+	jr $ra
+	
+getMin_ret_a0:
+	move $v0 $a0 # v0 = a0
+	jr $ra
 
 #################################################################
 # Student defined data section
@@ -534,7 +999,6 @@ calcPayloadSize:
 .data
 .align 2  # Align next items to word boundary
 
-newline: .asciiz "\n"
-
 #place all data declarations here
 
+newline: .asciiz "\n"
