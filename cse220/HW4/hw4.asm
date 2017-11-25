f47548572e00828d6599399481fef4f3f5049ce2
Mohsin Malik <mohsinmalik324@gmail.com>
2017-11-25 16:56:49 -0500

    Work on HW4 for CSE220

--- cse220/HW4/hw4.asm
+++ cse220/HW4/hw4.asm
@@ -285,23 +285,158 @@ start_game_end:
 # PART 2 FUNCTIONS
 ##############################
 
+# a0 - board
+# a1 - num rows
+# a2 - num cols
+# a3 - row
+# a4 - direction
 merge_row:
-    #Define your code here
-    ############################################
-    # DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
-    li $s0, 0x777
-    li $v0, 0x777
-    ############################################
-    jr $ra
+	lw $t0 0($sp) # load direction argument
+	
+	addi $sp $sp -32
+	sw $s6 28($sp)
+	sw $s5 24($sp)
+	sw $s4 20($sp)
+	sw $s3 16($sp)
+	sw $s2 12($sp)
+	sw $s1 8($sp)
+	sw $s0 4($sp)
+	sw $ra 0($sp)
+	
+	# s0 - board
+	# s1 - num rows
+	# s2 - num cols
+	# s3 - row
+	# s4 - direction
+	# s5 - col counter
+	# s6 - empty cell counter
+	
+	move $s0 $a0 # s0 = a0
+	move $s1 $a1 # s1 = a1
+	move $s2 $a2 # s2 = a2
+	move $s3 $a3 # s3 = a3
+	move $s4 $t0 # s4 = t0
+	
+	bltz $s3 merge_row_err # row < 0
+	bge $s3 $s1 merge_row_err # row >= num rows
+	li $t0 2 # t0 = 2
+	blt $s1 $t0 merge_row_err # num rows < 2
+	blt $s2 $t0 merge_row_err # num cols < 2
+	bltz $s4 merge_row_err # direction < 0
+	li $t0 1 # t0 = 1
+	bgt $s4 $t0 merge_row_err # direction > 1
+	
+	beqz $s4 merge_row_ltr # s4 == 0
+	
+merge_row_rtl:
+	
+	addi $s5 $s2 -1 # s5 = num of cols - 1
+	
+	# variables for the for loop:
+	# t0 - current cell addr
+	# t1 - current cell value
+	# t2 - previous cell value
+	# t3 - the value doubled
+	
+merge_row_rtl_loop:
+	blez $s5 merge_row_done # col counter <= 0
+	
+	move $a0 $s0 # board argument
+	move $a1 $s3 # row argument
+	move $a2 $s5 # col argument
+	move $a3 $s2 # number of cols argument
+	jal board_calc_addr
+	
+	move $t0 $v0 # t0 = v0
+	
+	lh $t1 0($t0) # load the current cell value
+	
+	li $t9 -1 # t9 = -1
+	beq $t1 $t9 merge_row_rtl_loop_cont # current cell value == -1
+	
+	lh $t2 -2($t0) # load the previous cell value
+	
+	bne $t1 $t2 merge_row_rtl_loop_cont # t1 != t2
+	
+	add $t3 $t1 $t1 # t3 = t1 * 2
+	sh $t3 0($t0)
+	sh $t9 -2($t0)
+	
+	addi $s5 $s5 -1 # col_counter
+	
+merge_row_rtl_loop_cont:
+	addi $s5 $s5 -1 # col_counter
+	
+	j merge_row_rtl_loop
 
+merge_row_ltr:
+	
+	li $s5 0 # col counter = 0
+	
+	# variables for the for loop:
+	# t0 - current cell addr
+	# t1 - current cell value
+	# t2 - next cell value
+	# t3 - the value doubled
+	# t8 - s2 minus 1
+	
+	addi $t8 $s2 -1 # t8 = num cols - 1
+	
+merge_row_ltr_loop:
+	bge $s5 $t8 merge_row_done # col counter >= num cols - 1
+	
+	move $a0 $s0 # board argument
+	move $a1 $s3 # row argument
+	move $a2 $s5 # col argument
+	move $a3 $s2 # number of cols argument
+	jal board_calc_addr
+	
+	move $t0 $v0 # t0 = v0
+	
+	lh $t1 0($t0) # load the current cell value
+	
+	li $t9 -1 # t9 = -1
+	beq $t1 $t9 merge_row_ltr_loop_cont # current cell value == -1
+	
+	lh $t2 2($t0) # load the next cell value
+	
+	bne $t1 $t2 merge_row_ltr_loop_cont # t1 != t2
+	
+	add $t3 $t1 $t1 # t3 = t1 * 2
+	sh $t3 0($t0)
+	sh $t9 2($t0)
+	
+	addi $s5 $s5 1 # col_counter++
+	
+merge_row_ltr_loop_cont:
+	addi $s5 $s5 1 # col_counter++
+	
+	j merge_row_ltr_loop
+	
+merge_row_err:
+	li $v0 -1 # v0 = -1
+	j merge_row_done
+	
+merge_row_done:
+	lw $ra 0($sp)
+	lw $s0 4($sp)
+	lw $s1 8($sp)
+	lw $s2 12($sp)
+	lw $s3 16($sp)
+	lw $s4 20($sp)
+	lw $s5 24($sp)
+	lw $s6 28($sp)
+	addi $sp $sp 32
+
+	jr $ra
+
+# a0 - board
+# a1 - num rows
+# a2 - num cols
+# a3 - col
+# a4 - direction
 merge_col:
-    #Define your code here
-    ############################################
-    # DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
-    li $s0, 0x777
-    li $v0, 0x777
-    ############################################
-    jr $ra
+	jr $ra
 
 shift_row:
     #Define your code here
--- cse220/HW4/hw4_main.asm
+++ cse220/HW4/hw4_main.asm
@@ -3,23 +3,34 @@
 .globl main
 
 main:
+	li $t1 0xffff0000
+	li $t2 2
+	li $t4 4
+	li $t8 8
+	
+	sh $t8 0($t1)
+	sh $t8 2($t1)
+	sh $t2 4($t1)
+	sh $t2 6($t1)
+	sh $t4 8($t1)
+	sh $t2 10($t1)
+	sh $t2 12($t1)
+	
+	#li $v0 10
+	#syscall
+
 	li $a0 0xffff0000
-	li $a1 4
-	li $a2 4
-	li $a3 0 # r1
+	li $a1 2
+	li $a2 7
+	li $a3 0 # row
+	li $t0 0 # direction
 	
-	li $t0 0 # c1
-	li $t1 3 # r2
-	li $t2 3 # c2
-	
-	addi $sp $sp -12
-	sw $t2 8($sp)
-	sw $t1 4($sp)
+	addi $sp $sp -4
 	sw $t0 0($sp)
 	
-	jal start_game
+	jal merge_row
 	
-	addi $sp $sp 12
+	addi $sp $sp 4
 
 	move $t0 $v0
 	li $v0 10
