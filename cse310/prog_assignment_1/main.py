3ceed7f7fe5edec996c07f0ae7d45ecde85ffa11
Mohsin Malik <mohsin@Mohsins-MacBook-Pro.local>
2018-09-24 14:06:01 -0400

    Finish programming assignment #1 cse310

---
+++ cse310/prog_assignment_1/main.py
@@ -0,0 +1,69 @@
+import socket
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = ''
+port = 8080
+serversocket.bind((host, port))
+serversocket.listen(1)
+print('Server running!')
+
+clientsocket,addr = serversocket.accept()
+print('Connection established!')
+
+d = {}
+protocol = 'HTTP/1.1'
+e200 = '200 OK\n'
+e220 = '220 UNSUPPORTED\n'
+e400 = '400 BAD_REQUEST\n'
+e404 = '404 NOT FOUND\n'
+bad_args = protocol + ' ' + e400
+
+while True:
+	msg = str(clientsocket.recv(256))
+	msg = msg[2:]
+	msg = msg[:-5]
+	#clientsocket.send((msg + '\n').encode())
+	if msg == 'QUIT':
+		clientsocket.send('Goodbye!\n'.encode())
+		break
+	args = msg.split(' ')
+	#print(args[0])
+	resp = protocol + ' ';
+	if args[0] == 'GET':
+		if len(args) == 2:
+			if args[1] in d:
+				resp += e200 + '\n' + d[args[1]] + '\n';
+			else:
+				resp += e404
+		else:
+			resp = bad_args
+	elif args[0] == 'PUT':
+		if len(args) == 3:
+			d[args[1]] = args[2]
+			resp += e200
+		else:
+			resp = bad_args
+	elif args[0] == 'DELETE':
+		if len(args) == 2:
+			try:
+				del d[args[1]]
+			except KeyError:
+				pass
+			resp += e200
+		else:
+			resp = bad_args
+	elif args[0] == 'CLEAR':
+		if len(args) == 1:
+			d = {}
+			resp += e200
+		else:
+			resp = bad_args
+	else:
+		resp += e220
+
+	clientsocket.send(resp.encode())
+
+clientsocket.close()
+serversocket.close()
+
+print('Goodbye!')
